d306c4eb7f2d05bb11e88ebd545e9f210f27f9fe
unknown <thomas.pennamen@gmail.com>
2020-03-22 10:45:27 +0100

    Create front directory

---
+++ front/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <home></home>
+  </div>
+</template>
+
+<script>
+import Home from './components/home'
+export default {
+  component: {Home}
+};
+</script>
+
+<style lang="css">
+@import 'style/style.css';
+</style>
