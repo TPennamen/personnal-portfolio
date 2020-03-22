9168e9b04391220b3da815c4c94e984644cbe92d
unknown <thomas.pennamen@gmail.com>
2020-03-22 18:02:57 +0100

    Added dist

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require("express");
 
 const api = express();
 
-
+api
+  .use(express.static("./dist"))
+  .use("*", (req, res) => res.sendFile("index.html", { root: "./dist" }));
 
 const server = http.createServer(api);
 
-server.listen(
-  console.log(`API Listenning on port 3000`)
-);
+server.listen(console.log(`API Listenning on port 3000`));
